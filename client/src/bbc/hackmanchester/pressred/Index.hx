0a130ae3099f92ce6846590d4dda3cded59e4e18
John Beech <bitbucket@mk25.net>
2013-10-26 19:45:25 +0100

    Aded the concept of a global EventBus (singleton). Moved tabs out of SplashScreen up to Index.

--- client/src/bbc/hackmanchester/pressred/Index.hx
+++ client/src/bbc/hackmanchester/pressred/Index.hx
@@ -5,6 +5,8 @@ import bbc.hackmanchester.pressred.controller.DataController;
 import bbc.hackmanchester.pressred.screen.Screen;
 import bbc.hackmanchester.pressred.screen.SplashScreen;
 import bbc.hackmanchester.pressred.model.GlobalTree;
+import bbc.hackmanchester.pressred.singleton.EventBus;
+import bbc.hackmanchester.pressred.ui.TabUI;
 import flash.display.Sprite;
 
 /**
@@ -14,30 +16,41 @@ import flash.display.Sprite;
 class Index
 {
 	public var artwork:Sprite;
-	public var tree:GlobalTree;
+	public var screens:Sprite;
 	
+	public var tree:GlobalTree;
 	public var screenController:ScreenController;
 	public var dataController:DataController;
 	
+	public var tabs:TabUI;
 	public var splashScreen:SplashScreen;
 
 	public function new() 
 	{
 		artwork = new Sprite();
+		screens = new Sprite();
 	}
 	
 	public function setup()
 	{
+		EventBus.setup();
+		
 		tree = new GlobalTree();
 		
+		tabs = new TabUI();
+		tabs.setup();
+		artwork.addChild(tabs.artwork);
+		
 		screenController = new ScreenController();
-		screenController.setup(artwork);
+		screenController.setup(screens);
 		
 		dataController = new DataController();
 		dataController.setup(tree);
 		
 		splashScreen = new SplashScreen();
 		splashScreen.setup(tree.hackTimer);
+		
+		artwork.addChild(screens);
 	}
 	
 	public function resize()
---
+++ client/src/bbc/hackmanchester/pressred/singleton/EventBus.hx
@@ -0,0 +1,21 @@
+package bbc.hackmanchester.pressred.singleton;
+
+import bbc.hackmanchester.pressred.core.Signal;
+
+class EventBus
+{
+	public static var showSplashScreen:Signal;
+	public static var showStreamScreen:Signal;
+	public static var showPersonScreen:Signal;
+	public static var showStarScreen:Signal;
+	public static var showAwardsScreen:Signal;
+
+	public static function setup()
+	{
+		showSplashScreen = new Signal();
+		showStreamScreen = new Signal();
+		showPersonScreen = new Signal();
+		showStarScreen = new Signal();
+		showAwardsScreen = new Signal();
+	}
+}
