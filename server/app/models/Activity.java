d2b4797ee04403c69c6f17a6437285c0bb5f76d3
roberton <rnerici@gmail.com>
2013-10-27 06:48:47 +0000

    Talks to github for the [activity] end point. Doesn't do anything _particularly_ useful yet, but it is a good start

--- server/app/models/Activity.java
+++ server/app/models/Activity.java
@@ -1,13 +1,25 @@
 package models;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import play.libs.WS;
+
 public class Activity {
     public int numberOfCommits;
 
     public void update() {
-        numberOfCommits = (int) Math.random() % 100 + 1;
-    }
-
-    public void foo() {
 //        GET /repos/:owner/:repo/events
+//        https://api.github.com/repos/Markavian/HackManchester/events
+        WS.WSRequest getRequest = WS.url("https://api.github.com/repos/Markavian/HackManchester/events");
+        WS.HttpResponse result = getRequest.get();
+
+        System.out.println(result.getStatus());
+        System.out.println(result.getStatusText());
+        JsonElement contentJson = result.getJson();
+
+        if (contentJson.isJsonArray()) {
+            JsonArray jsonArray = contentJson.getAsJsonArray();
+            numberOfCommits = jsonArray.size();
+        }
     }
 }
